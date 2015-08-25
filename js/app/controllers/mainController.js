cf733e9952e52865d033a91ed85f8ef5e92e472c
Christophe Boucaut <boucaut.christophe@gmail.com>
2015-08-25 12:38:57 +0200

    Update mainController.js

--- js/app/controllers/mainController.js
+++ js/app/controllers/mainController.js
@@ -3,13 +3,28 @@
 
     angular.module('ffxivChasse').controller('mainController', [
         '$scope',
+        '$compile',
         '$mdDialog',
         'cbLocalStorage',
         'maps',
-        function ($scope, $mdDialog, cbLocalStorage, maps) {
+        function ($scope, $compile, $mdDialog, cbLocalStorage, maps) {
             var NAMESPACE_FFIXV_CHASSE = 'ffxivChasse';
             var KEY_STORAGE_CURRENT_MOBS = 'currentMobs';
             var basenameFileMap = 'img/maps/';
+
+            var colorsCircles = [
+                {background: '255, 0, 0', border: 'red'},
+                {background: '0, 0, 255', border: 'blue'},
+                {background: '0, 255, 0', border: 'green'},
+                {background: '200, 4, 208', border: '#C804D0'},
+                {background: '41, 133, 140', border: '#29858C'},
+                {background: '218, 90, 15', border: '#DA5A0F'},
+                {background: '0, 0, 0', border: 'black'},
+                {background: '255, 255, 255', border: 'white'},
+                {background: '128, 128, 128', border: 'grey'},
+                {background: '255, 246, 0', border: '#FFF600'},
+                {background: '255, 69, 152', border: '#FF4598'}
+            ];
 
             $scope.currentMobs = cbLocalStorage.getItem(NAMESPACE_FFIXV_CHASSE, KEY_STORAGE_CURRENT_MOBS, {});
             $scope.exportJson = '';
@@ -34,27 +49,98 @@
                 return countSelected;
             };
 
-            $scope.openMap = function (map, mob, $event) {
+            $scope.openMap = function (map, mob, $event, idsMobsSelected) {
                 var pathFile = basenameFileMap+map.fileName;
-                var positions = mob.positions;
-                var title = map.name+' - '+mob.name;
+                var title = map.name;
+                if (idsMobsSelected === undefined) {
+                    title +=' - '+mob.name;
+                }
 
                 $mdDialog.show({
                     template: templateDialog(),
                     parent: angular.element(document.body),
                     controller: [
                         '$scope',
-                        function ($scope) {
+                        '$timeout',
+                        function ($scope, $timeout) {
                             $scope.closeDialog = $mdDialog.hide;
 
                             $scope.pathFile = pathFile;
                             $scope.title = title;
-                            $scope.positions = positions;
+
+                            $timeout(function () {
+                                angular.element('#dialogbox-map').bind("load", setPositions);
+                            }, 0);
+
+                            $scope.$on('resize', setPositions);
                         }
                     ],
                     targetEvent: $event,
-                    clickOutsideToClose: true
+                    clickOutsideToClose: true,
+                    onComplete: function () {
+                        // Hack to remove the css added by angular-material to align the dialog box when the user scroll the page.
+                        var dialogBox = angular.element('.md-dialog-container');
+                        dialogBox
+                            .css('top', '0')
+                            .css('height', 'inherit');
+                        dialogBox.parent().css('top', '0');
+
+                        angular.element('#dialogbox-map').parent().addClass('dialogbox-opened');
+
+                        setPositions();
+                    }
                 });
+
+                function setPositions() {
+                    var imgElement = angular.element('#dialogbox-map');
+                    var container = imgElement.parent();
+                    container.children('.position').remove();
+
+                    if (idsMobsSelected) {
+                        var mobsDisplayed = 0;
+                        angular.forEach(map.mobs, function (mob, keyMob) {
+                            if (idsMobsSelected.hasOwnProperty(keyMob) && idsMobsSelected[keyMob]) {
+                                addCircle(
+                                    map,
+                                    mob,
+                                    colorsCircles.length > mobsDisplayed ? colorsCircles[mobsDisplayed] : colorsCircles[colorsCircles.length-1]
+                                );
+                                mobsDisplayed++;
+                            }
+                        });
+                    } else {
+                        addCircle(map, mob, colorsCircles[0]);
+                    }
+
+                    function addCircle(map, mob, colorUsed) {
+                        var positionMax = map.positionMax - 1;
+                        var imgWidth = imgElement.width();
+                        var imgHeight = imgElement.height();
+
+                        angular.forEach(mob.positions, function (position, key) {
+                            var ratioWidth = imgElement.width() / positionMax;
+                            var ratioHeight = imgElement.height() / positionMax;
+                            var posX = position.x * ratioWidth;
+                            var posY = position.y * ratioHeight;
+
+                            var positionElement = angular.element('<div>')
+                                .addClass('position')
+                                .css('width', ratioWidth+'px')
+                                .css('height', ratioHeight+'px')
+                                .css('background', 'rgba('+colorUsed.background+', 0.5)')
+                                .css('border', '1px solid '+colorUsed.border)
+                                .css('position', 'absolute')
+                                .css('border-radius', (imgElement.width() / positionMax)+'px')
+                                .css('left', (imgElement.position().left + posX - (ratioWidth / 2))+"px")
+                                .css('top', (imgElement.position().top + posY - (ratioHeight / 2))+"px");
+                            positionElement.append(angular.element('<md-tooltip>').attr('md-direction', 'right').text(mob.name));
+
+                            positionElement = $compile(positionElement)($scope);
+
+                            container.append(positionElement);
+                        });
+                    }   
+                }
 
                 $event.stopPropagation();
             };
@@ -102,7 +188,7 @@
                             </md-toolbar>\
                             <md-dialog-content style='position: relative; height: 100%;'>\
                                 <div style='position: absolute; height: 100%; top: 0; left: 0; right: 0; bottom: 0; text-align: center;'>\
-                                    <img ng-src='{{ pathFile }}' style='max-width: 100%; max-height: 100%;'>\
+                                    <img id='dialogbox-map' ng-src='{{ pathFile }}' style='max-width: 100%; max-height: 100%;'>\
                                 </div>\
                             </md-dialog-content>\
                         </form>\
